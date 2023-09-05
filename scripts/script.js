d12d1a33c41e1e89dfe96ff3a179c4cb71bae169
Roselene Gabun <roselenegabun@gmail.com>
2023-09-05 03:43:42 -0700

    add tabbed interface

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+window.addEventListener('load', closeNav);
+
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidebar").style.width = "200px";
@@ -9,3 +11,20 @@ function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+$(function () {
+  // Script for submenu:
+  $('.hwParts').hide();
+  $('#description').show();
+  $('#descriptionBtn').addClass('activeSubMenuButton');
+
+  $('.btn-class').click(function () {
+      // Hide all sections
+      $('.hwParts').hide();
+      $('.btn-class').removeClass('activeSubMenuButton');
+      
+      // Show the selected section
+      $('#' + $(this).attr('data-target')).show();
+      $(this).addClass('activeSubMenuButton');
+  });
+});
