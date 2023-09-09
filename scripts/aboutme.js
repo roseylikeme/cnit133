3f5c9eae98cef246b2ee4bbcde647a28d9d8b857
Roselene Gabun <roselenegabun@gmail.com>
2023-09-09 11:22:30 -0700

    add animations on about page

---
+++ scripts/aboutme.js
@@ -0,0 +1,21 @@
+$(function (){
+    $('#startAnimationBtn').click(function (){
+        console.log('start animation button clicked')
+        initialFontSize = $('.main-heading').css('font-size');
+        initialColor = $('.main-heading').css('color');
+        // Grow the font size
+        $('.main-heading').animate({fontSize: "50px"}, "slow", function(){
+            // Go back to initial font size
+            $(this).animate({fontSize: initialFontSize, color: initialColor }, "slow");
+        })
+        // Change color
+        $('.main-heading').css({color: "red"})
+        
+        setTimeout(function (){
+            $('.main-heading').css({color: initialColor})
+        })
+
+        $("#my-image").css("border", "5px solid white");
+        $("#my-image").animate({ border: "5px solid white" }, "slow");
+    })
+})
