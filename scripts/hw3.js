e333dbb293e5c0a7de5d57ec1f8e2cb233bc9e4b
Roselene Gabun <roselenegabun@gmail.com>
2023-09-30 23:40:24 -0700

    Part 2 is finished

--- scripts/hw3.js
+++ scripts/hw3.js
@@ -17,6 +17,36 @@ $(document).ready(function () {
         clearInputsAndError();
     });
 
+    $('#clearBtnPt2').click(function () {
+      // Clear salesperson input
+      $('#salesperson').val('');
+
+      // Clear item quantity sold input fields
+      $('#item1').val('');
+      $('#item2').val('');
+      $('#item3').val('');
+      $('#item4').val('');
+
+      // Clear quantity sold output fields
+      $('#item1-sold').val('');
+      $('#item2-sold').val('');
+      $('#item3-sold').val('');
+      $('#item4-sold').val('');
+
+      // Clear item total output fields
+      $('#item1-total').val('');
+      $('#item2-total').val('');
+      $('#item3-total').val('');
+      $('#item4-total').val('');
+
+      // Clear total sold and total earnings fields
+      $('#total-sold').val('');
+      $('#total-earnings').val('');
+
+      // Clear any previous error message
+      $('#resultsPt2').text('').hide();
+  });
+
     // Part two
     $('#salesperson').tooltip({
       content: "Please make sure that the salesperson's name is spelled correctly",
@@ -30,16 +60,16 @@ $(document).ready(function () {
           item3: parseFloat($('#item3Price').text().substring(1)),
           item4: parseFloat($('#item4Price').text().substring(1)),
       };
-    
+  
       const itemsSoldInputValues = {
           item1: parseInt($('#item1').val()),
           item2: parseInt($('#item2').val()),
           item3: parseInt($('#item3').val()),
           item4: parseInt($('#item4').val()),
       };
-    
+  
       let errorMessage = '';
-    
+  
       // Check for empty or negative input values
       for (const item in itemsSoldInputValues) {
           if (itemsSoldInputValues[item] < 0 || isNaN(itemsSoldInputValues[item])) {
@@ -47,29 +77,33 @@ $(document).ready(function () {
               break;
           }
       }
-    
+  
       if (errorMessage) {
           $('#resultsPt2').text(errorMessage).show();
       } else {
           const itemTotals = {};
           let totalSold = 0;
-    
+  
           for (const item in itemsSoldInputValues) {
               itemTotals[item] = (itemsSoldInputValues[item] * itemPrices[item]).toFixed(2);
               totalSold += parseFloat(itemTotals[item]);
+  
+              // Update quantity sold input fields (notice the corrected IDs)
+              $('#' + item + '-sold').val(itemsSoldInputValues[item]);
               $('#' + item + '-total').val(itemTotals[item]);
           }
-    
+  
           const totalEarnings = (250 + (totalSold * 0.09)).toFixed(2);
-    
+  
           // Update fields
           $('#total-sold').val(totalSold.toFixed(2));
           $('#total-earnings').val(totalEarnings);
-    
+  
           // Clear any previous error message
           $('#resultsPt2').text('').hide();
       }
-    });
+  });
+  
   
 });
 
