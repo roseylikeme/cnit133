442730c4ab5df4afbafaa9a529529adc1b648a86
Roselene Gabun <roselenegabun@gmail.com>
2023-11-20 23:27:55 -0800

    add icon...

--- m-hw4/sw.js
+++ m-hw4/sw.js
@@ -1,23 +1,22 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('my-pwa-cache').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/app.js',
-                '/lightblue.jpg',
-                '/lightgold.jpg',
-                '/icon.png',
-            ]);
-        })
-    );
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open("my-pwa-cache")
+      .then((cache) => {
+        return cache.addAll([
+          "/",
+          "/app.js",
+          "/icon.png",
+          "/index.html",
+          "/lightblue.jpg",
+          "/lightgold.jpg",
+          "/style.css",
+          "/sw.js",
+          "/manifest.json",
+        ]);
+      })
+      .catch((error) => {
+        console.error("Cache installation failed:", error);
+      })
+  );
 });
